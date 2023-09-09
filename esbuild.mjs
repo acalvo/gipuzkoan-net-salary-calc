0e6815b266b4bec5410813305a35a56cf134790f
Alberto Calvo <git@albertocalvo.com>
2023-09-09 21:51:22 +0200

    chore: bump dependencies

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,28 +1,29 @@
-import child_process from 'child_process';
-import esbuild from 'esbuild';
-import fs from 'fs';
+import * as esbuild from 'esbuild';
 import htmlPlugin from '@chialab/esbuild-plugin-html';
 
 const devServer = process.argv.slice(2).includes('--serve');
 
 const OUT_DIR = devServer ? './tmp' : './dist';
 
-if (devServer) {
-  const server = child_process.exec(`npx http-server -c-1 ${OUT_DIR}`);
-  process.on('exit', () => server.kill());
-}
-
-fs.rmSync(OUT_DIR, { recursive: true, force: true });
-
-await esbuild.build({
+const options = {
   entryPoints: ['src/index.html'],
   outdir: OUT_DIR,
   bundle: true,
   minify: !devServer,
   sourcemap: devServer,
-  watch: devServer,
   chunkNames: devServer ? 'bundle' : 'bundle.[hash]',
   plugins: [
     htmlPlugin(),
   ],
-});
+};
+
+if (!devServer) {
+  await esbuild.build(options);
+} else {
+  let ctx = await esbuild.context(options);
+  let { port } = await ctx.serve({
+    servedir: OUT_DIR,
+    port: 8080,
+  });
+  console.log(`Running on http://localhost:${port}`);
+}
